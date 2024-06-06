388f16085c16b849bbfdf44796b0a68b0b6b54b8
Hadeed Ahmad <hadeedji@gmail.com>
2024-06-07 03:25:45 +0400

    Finish design

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,20 @@
 import * as RadioGroup from "@radix-ui/react-radio-group";
+import * as Checkbox from "@radix-ui/react-checkbox";
+
+import icon_checkbox_checked from "../assets/icon-checkbox-checked.svg";
+import icon_checkbox_unchecked from "../assets/icon-checkbox-unchecked.svg";
+import { useState } from "react";
 
 const InputField = ({ label, type = "text", required = false, ...props }) => {
+  const inputProps = {
+    className:
+      "block w-full cursor-pointer rounded-lg border-[1px] border-grey-500 px-300 py-150 text-body-md focus:border-green-600 group-hover:border-green-600",
+    name: label.toLowerCase().replace(/ /g, "_"),
+    required: required,
+    autoComplete: "off",
+    ...props,
+  };
+
   return (
     <label className="group block cursor-pointer space-y-100">
       <div className="space-x-100">
@@ -9,19 +23,18 @@ const InputField = ({ label, type = "text", required = false, ...props }) => {
         </span>
         {required && <span className="text-body-sm text-green-600">*</span>}
       </div>
-      <input
-        className="block w-full cursor-pointer rounded-lg border-[1px] border-grey-500 px-300 py-150 text-body-md focus:border-green-600 group-hover:border-green-600"
-        name={label.toLowerCase().replace(/ /g, "_")}
-        required={required}
-        type={type}
-        autoComplete="off"
-        {...props}
-      />
+      {type == "textarea" ? (
+        <textarea {...inputProps} />
+      ) : (
+        <input type={type} {...inputProps} />
+      )}
     </label>
   );
 };
 
 export default () => {
+  const [checkbox, setCheckbox] = useState(false);
+
   return (
     <div className="min-h-dvh bg-green-200 px-200 py-400">
       <div className="rounded-2xl bg-white p-300 text-grey-900">
@@ -58,7 +71,36 @@ export default () => {
                 ))}
               </RadioGroup.Root>
             </div>
+
+            <InputField label="Message" type="textarea" rows="4" required />
           </div>
+
+          <div className="my-500 flex items-center">
+            <Checkbox.Root
+              className="flex-shrink-0"
+              checked={checkbox}
+              onCheckedChange={setCheckbox}
+              required
+              id="c1"
+            >
+              <img
+                width="24px"
+                height="24px"
+                src={checkbox ? icon_checkbox_checked : icon_checkbox_unchecked}
+              />
+            </Checkbox.Root>
+            <label
+              className="mx-200 cursor-pointer text-body-sm text-grey-900"
+              htmlFor="c1"
+            >
+              I consent to being contacted by the team{" "}
+              <span className="text-green-600">*</span>
+            </label>
+          </div>
+
+          <button className="w-full rounded-lg bg-green-600 py-200 text-center text-body-md font-bold text-white hover:bg-grey-900">
+            Submit
+          </button>
         </form>
       </div>
     </div>
